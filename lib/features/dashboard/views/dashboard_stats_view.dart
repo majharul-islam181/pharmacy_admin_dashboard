364818f2c6ec479cf868644539e8a9a979e53d73
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-12-29 17:49:15 +0600

    refactor: Remove commented-out code from DashboardStatsView for cleaner implementation

--- lib/features/dashboard/views/dashboard_stats_view.dart
+++ lib/features/dashboard/views/dashboard_stats_view.dart
@@ -5,57 +5,6 @@ import '../../../core/constants/app_dimensions.dart';
 import '../../../core/utils/responsive_utils.dart';
 import '../view_models/dashboard_stats_view_model.dart';
 import 'widgets/responsive_stats_grid.dart';
-
-// class DashboardStatsView extends StatelessWidget {
-//   const DashboardStatsView({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer<DashboardStatsViewModel>(
-//       builder: (context, viewModel, child) {
-//         return Container(
-//           width: double.infinity,
-//           padding: EdgeInsets.all(
-//             ResponsiveUtils.isMobile(context)
-//                 ? AppDimensions.paddingMedium
-//                 : AppDimensions.paddingLarge,
-//           ),
-//           child: viewModel.isLoading
-//               ? const Center(
-//                   child: CircularProgressIndicator(),
-//                 )
-//               : Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     // Page Title (Optional)
-//                     if (ResponsiveUtils.isDesktop(context))
-//                       const Padding(
-//                         padding: EdgeInsets.only(bottom: AppDimensions.paddingMedium),
-//                         child: Text(
-//                           'Dashboard Overview',
-//                           style: TextStyle(
-//                             fontSize: 24,
-//                             fontWeight: FontWeight.w600,
-//                             color: AppColors.textDark,
-//                           ),
-//                         ),
-//                       ),
-
-//                     // Stats Cards
-//                     ResponsiveStatsGrid(
-//                       cards: viewModel.statsCards,
-//                     ),
-
-//                     // Add more dashboard content here if needed
-//                     const SizedBox(height: AppDimensions.paddingLarge),
-//                   ],
-//                 ),
-//         );
-//       },
-//     );
-//   }
-// }
-
 import 'package:easy_localization/easy_localization.dart';
 import '../../../core/localization/locale_keys.dart';
 import 'widgets/dashboard_tables_section.dart';
